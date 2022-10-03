4c06fcc0fe52769fca1a852c066f589c77e35638
Gabriel Parreiras <gparreiras46@gmail.com>
2022-10-02 21:30:51 -0300

    fetch api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 
 export async function getStaticProps() {
@@ -11,13 +12,15 @@ export async function getStaticProps() {
 
     const data = await res.json()
 
-    // data.results.forEach((item, index) => {
-    //     item.id = index + 1
-    // });
+    data.results.forEach((item, index) => {
+        item.id = index + 1
+    });
+
+
 
     return{
       props:{
-        pokemons: data.results
+        pokemons: data.results,
       }
     }
 
@@ -31,7 +34,10 @@ export default function Home({ pokemons }) {
   return (
     <div className={styles.container}>
       {pokemons.map(i=>(
-        <h3>{i.name}</h3>
+        <div className={styles.cardItem}>
+          <h3>{i.name}</h3>
+          <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i.id}.png`} width='100px' height='100px'/>
+        </div>
       ))}
     </div>
   )
